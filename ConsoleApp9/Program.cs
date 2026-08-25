e7143db620fe7df41125737d90fb8b61d1b139ef
User <User@WINDOWS-QPMQC0H>
2026-08-26 02:15:46 +0400

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,28 @@
+﻿//ARRAY METHODS
+
+//int[]arr = {5, 10, 3, 8, 9, 1};
+//Array.Sort(arr); ARRAYI SIRALAMAQ KICIKDEN BOYUYE
+//Array.Reverse(arr); TERSINE CEVIRMEK
+// Array.Clear(arr, 2, 2); SADECE ICINDEKI ARRAYI TEMIZLEYIR YENI ICI 0 QALIR
+//arr = Array.ConvertAll(arr, x => x * 2); arrayin icindekileri deyisdirir
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    Console.WriteLine(arr[i]);
+//}
+// Array.Copy(arr, newArr, 3) kopyaliyir, yeni arrayimizdan 3 denesini yeni arraya atacaq
+// Array.Resize (ref arr, 10); arrayimizin uzunlugunu 10 eleyir
+// var filteredArr = Array.FindAll(arr, x => x > 5); yeni array yaradir arrayin icinde 5den boyuk olanlari verir
+// var result = Array.Find(arr, x => x > 5); ilk qabagina cixani verir
+//var result = arr.Sum(x => x); arrayin icindekileri toplayir
+//Console.WriteLine(result);
+//var max = arr.Max(x => x); maxi verir
+//Console.WriteLine(max);
+//var min = arr.Min(x => x); mini verir
+//Console.WriteLine(min);
+//var ave = arr.Average(x => x); ortalamani verir
+//Console.WriteLine(ave);
+//var count = arr.Count(x => x > 5); arrayin icinde 5den boyuk olanlarin sayini verir
+//Console.WriteLine(count);
+//var distinct = arr.Distinct().ToArray; tekrarlanmayan ededler
+//Console.WriteLine(distinct);
+//var distinctCount = arr.Distinct().Count(); tekrarlanmayanlarin sayi
